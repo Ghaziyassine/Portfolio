f70498d34df7cb0e941ff359fc727b8c2823a332
Ghaziyassine <ghaziyassine241@gmail.com>
2024-03-02 14:51:35 +0100

    CV

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,123 +1,126 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-
-  :root{
+  :root {
     --pink: #E31F71;
-    --black: #212121;
+    --black: #000000; /* Normal black color */
     --green: #23ce6b;
     --blue: #016fb9;
     scroll-padding-top: 10rem;
 
-    &.light{
-
-      body{
+    &.light {
+      body {
         transition: 0.5s;
         background-color: #f5f5f5;
         color: var(--black);
       }
 
-      .logo{
+      .logo {
         color: var(--black);
       }
 
-      header.header-fixed{
+      header.header-fixed {
         transition: 0.5s;
         background-color: #f5f5f550;
-        a{
+        a {
           transition: 0.5s;
           color: black;
         }
-        .menu,.menu:before, .menu:after{
-          background-color: var(--black); 
+        .menu,
+        .menu:before,
+        .menu:after {
+          background-color: var(--black);
         }
-        .menu.active{
-          background-color: rgba(555,555,555,0);
+        .menu.active {
+          background-color: rgba(555, 555, 555, 0);
         }
       }
 
-      footer.footer{
+      footer.footer {
         transition: 0.5s;
-        background-color: rgba(0,0,0,0.1);
+        background-color: rgba(0, 0, 0, 0.1);
         color: var(--black);
       }
 
-      form{
-        input,textarea{
+      form {
+        input,
+        textarea {
           transition: 0.5s;
           border: solid 1px var(--black);
           color: var(--black);
-          &::placeholder{
+          &::placeholder {
             transition: 0.5s;
             color: var(--black);
           }
         }
       }
-
     }
   }
 
-
-
-  ul, li {
+  ul,
+  li {
     text-decoration: none;
     list-style: none;
     margin: 0;
-    padding:0;
+    padding: 0;
   }
 
-  *{
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
 
-  html{
+  html {
     font-size: 62.5%;
   }
 
-  body{
+  body {
     font-size: 1.6rem;
     -webkit-font-smoothing: antialiased;
     background-color: var(--black);
-    color: #FFFF;
+    color: #ffff;
   }
 
-  body, input, textarea, button{
+  body,
+  input,
+  textarea,
+  button {
     font-family: 'Red Hat Display', sans-serif;
     font-weight: 400;
   }
 
-  a{
+  a {
     text-decoration: none;
   }
 
-  button, .button{
+  button,
+  .button {
     border: none;
     cursor: pointer;
     background-color: var(--green);
-    color: #FFFF;
+    color: #ffff;
     border-radius: 2rem;
     font-weight: 500;
     transition: filter 0.25s;
-    &:hover{
+    &:hover {
       filter: brightness(0.8);
     }
   }
 
-  button:disabled, .button:disabled{
+  button:disabled,
+  .button:disabled {
     filter: brightness(0.8);
     cursor: not-allowed;
   }
 
-
-  .logo{
+  .logo {
     font-size: 3rem;
-    color: #FFFF;
+    color: #ffff;
     // &::first-letter{
     //   color: var(--green);
     // }
   }
-  
+`;
 
-`
+export default GlobalStyle;
