03790b7c6ccc0afb9bd41e64493b68392d7e058d
Ghaziyassine <ghaziyassine241@gmail.com>
2024-03-02 02:14:30 +0100

    first commit

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,28 @@
+import { Container } from "./styles";
+import emailIcon from "../../assets/email-icon.svg";
+import phoneIcon from "../../assets/phone-icon.svg"
+import { Form } from "../Form/Form";
+
+
+export function Contact(){
+
+  return(
+    <Container id="contact">
+      <header>
+        <h2>Contact</h2>
+        <p>Ready to get started on your project? </p>
+        <p>Contact me now for a Free consultation.</p>
+      </header>
+      <div className="contacts">
+        <div>
+        <a href="mailto:gyassine241@gmail.com"><img src={emailIcon} alt="Email" /></a> 
+          <a href="mailto:gyassine241@gmail.com">gyassine241@gmail.com</a>
+        </div>
+        <div>
+        <a href="tel:+919630576848"><img src={phoneIcon} alt="Phone No" /></a>
+          <a href="tel:+919630576848">(+212) 645708872</a>
+        </div>  
+      </div>
+    </Container>
+  )
+}
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,56 @@
+import { Container } from './styles'
+
+import reactIcon from '../../assets/react-icon.svg'
+import linkedin from '../../assets/linkedin.svg'
+import githubIcon from '../../assets/github.svg'
+import whatsapp from '../../assets/whatsapp.svg'
+import telegram from '../../assets/telegram.svg'
+import instagramIcon from '../../assets/instagram.svg'
+// import discordIcon from '../../assets/discord.png'
+
+export function Footer() {
+  return (
+    <Container className="footer">
+      
+      <div>
+        <p>
+          This Website was made with <img src={reactIcon} alt="React" />
+          {/* <span>❤️</span> */}
+        </p>
+      </div>
+
+      <div className="social-media">
+        <a
+          href="https://www.linkedin.com/in/yassine-ghazi-b18417258?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedin} alt="Linkedin" />
+        </a>
+        <a
+          href="https://github.com/Ghaziyassine/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={githubIcon} alt="GitHub" />
+        </a>
+       
+        <a
+          href="https://t.me/CodeVinayak"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={telegram} alt="telegram" />
+        </a>
+        <a
+          href="https://www.instagram.com/yassin_ghz/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={instagramIcon} alt="Instagram" />
+        </a>
+     
+      </div>
+    </Container>
+  )
+}
